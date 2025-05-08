7704acd6d65d899a74f198629978122b35ce954d
ahmedHarb <aharb@averroes.ai>
2025-05-08 03:06:03 +0300

    fix: Resolve tanstack router TS issue

---
+++ src/routes/about.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/about")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  return <div className="text-5xl">Hello "/about"!</div>;
+}
